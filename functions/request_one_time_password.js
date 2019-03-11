ecb82938a880f440a1d0aee9e4f1e61b5aa308aa
andres-thefirst <darkshadowskull@gmail.com>
2019-03-10 22:24:00 -0600

    One time password

---
+++ functions/request_one_time_password.js
@@ -0,0 +1,28 @@
+const admin = require('firebase-admin');
+const twillio = require('./twilio');
+
+module.exports = function (req, res) {
+  if (!req.body.phone) {
+    return res.status(422).send({ error: 'You must provide a phone number'});
+  }
+
+  const phone = String(req.body.phone).replace(/[^\d]/g, '');
+  admin.auth().getUser(phone)
+    .then(userRecord => {
+      const code = Math.floor((Math.random() * 8999 + 1000));
+      twillio.messages.create({
+        body: 'Your code is ' + code,
+        to: '+'+phone,
+        from: '+12053016773',
+      }, (err) => {
+        if (err) { return res.status(422).send(err + ' sending'); }
+
+        admin.database().ref('users/' + phone)
+          .update({ code: code, codeValid: true }, () => {
+              res.send({ success: true });
+          });
+      });
+      return;
+    })
+    .catch(err => res.status(422).send({ error: err + ' catch' }));
+};
